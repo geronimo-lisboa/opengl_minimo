45d5a399eace54caec12f7c04aff253f32e3bafb
luciano geronimo <erikaeluciano2016@gmail.com>
2017-07-29 14:03:48 -0300

    Shader separado

--- rendering3d.h
+++ rendering3d.h
@@ -5,34 +5,9 @@
 #include <string>
 #include <map>
 #include <itkImage.h>
+#include "shader.h"
 using namespace std;
-class Shader
-{
-private:
-	GLuint programId, vsId, fsId;
-	map<string, GLuint> attributes;
-	map<string, GLuint> uniforms;
-
-	std::string ReadShaderFile(std::string path);
-	//Extrai a lista de atributos e uniformes do programa dado. Os pares nome/localiza��o no shader s�o retornados
-	//nos mapas passados como par�metro.
-	void introspectProgram(GLuint programId, map<string, GLuint> &attributes, map<string, GLuint> &uniforms);
-	//Gera o log do shader. A responsabilidade de deletar a string alocada � do invocador da fun��o.
-	string GetShaderInfoLog(GLuint object, PFNGLGETSHADERIVPROC glGet__iv, PFNGLGETSHADERINFOLOGPROC glGet__InfoLog);
-	//Cria um shader do tipo dado a partir do c�digo fonte dado.
-	GLuint MakeShader(GLenum type, std::string source);
-	//Cria o programa de shader a partir dos shaders dados.
-	GLuint MakeProgram(GLuint vertex_shader, GLuint fragment_shader);
-public:
-	Shader(string vsPath, string fsPath);
-	Shader(stringstream vsStream, stringstream fsStream);
-	~Shader();
-	GLuint GetUniform(string name);
-	GLuint GetAttribute(string name);
-	GLuint GetProgramId();
-	void UseProgram();
-};
-
+using namespace my3d;
 class Object3d
 {
 protected:
